958d7f6670fb1b14a264bdbfd2fd4eb7ca47de4f
Sam Chu <samchu1997@outlook.com>
2021-07-12 15:30:27 +0800

    modified 'switchtotag' default config

--- config.def.h
+++ config.def.h
@@ -27,8 +27,8 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     switchtotag    isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            0,             1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,             0,           -1 },
+	{ "Gimp",     NULL,       NULL,       1 << 6,       1,             1,           -1 },
+	{ "Firefox",  NULL,       NULL,       1 << 2,       1,             0,           -1 },
 };
 
 /* layout(s) */
