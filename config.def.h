d2c328477fba915ffeb8aabb86f5c9eec6a9bb07
Sam Chu <samchu1997@outlook.com>
2021-07-12 16:20:08 +0800

    modified 'awesomebar' default config

--- config.def.h
+++ config.def.h
@@ -87,8 +87,8 @@ static Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-	{ MODKEY,                       XK_s,      show,           {0} },
-	{ MODKEY,                       XK_h,      hide,           {0} },
+	{ MODKEY|ShiftMask,             XK_c,      show,           {0} },
+	{ MODKEY,                       XK_c,      hide,           {0} },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
